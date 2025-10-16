a0a7a0214aa961df83e6cbf7b4c118d6419508ff
L-Surkov <ilya.surkov11@gmail.com>
2025-10-16 09:21:19 +0300

    updates for test after check

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -1,11 +1,10 @@
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.Selectors;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,7 +17,7 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
@@ -28,7 +27,7 @@ public class PracticeFormTest {
         $("#firstName").setValue("Ilya");
         $("#lastName").setValue("Surkov");
         $("#userEmail").setValue("el@gmail.com");
-        $("label[for='gender-radio-1']").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("8929888554");
         $("#dateOfBirthInput").click();
         String day = "010";
@@ -37,17 +36,12 @@ public class PracticeFormTest {
         $(".react-datepicker__year-select").selectOption(String.valueOf(year));
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__day--" + day).click();
-        $("#subjectsInput").click();
-        $("#subjectsInput").setValue("Hi");
-        Selenide.sleep(500);
-        $$(".subjects-auto-complete__option").findBy(text("History")).click();
-        $("label[for='hobbies-checkbox-1']").click();
+        $("#subjectsInput").setValue("History").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("images/imagefortest.png");
         $("#currentAddress").setValue("г. Пенза, ул. Красная 64, кв. 166");
-        $(Selectors.byText("Select State")).click();
-        $(Selectors.byText("NCR")).click();
-        $(Selectors.byText("Select City")).click();
-        $(Selectors.byText("Noida")).click();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
         $(".modal-body").shouldHave(text("Ilya"));
         $(".modal-body").shouldHave(text("Surkov"));
